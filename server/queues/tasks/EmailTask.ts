2a4c214e8f758dc6f0bced3be19292d719df5e1f
Evan Zhu <lifereduce@gmail.com>
2024-07-17 18:33:00 +0800

    Add more debug infor when email task failed

--- server/queues/tasks/EmailTask.ts
+++ server/queues/tasks/EmailTask.ts
@@ -1,21 +1,26 @@
-import emails from "@server/emails/templates";
-import BaseTask from "./BaseTask";
+import emails from '@server/emails/templates';
+import BaseTask from './BaseTask';
+import Logger from '@server/logging/Logger';
 
 type Props = {
-  templateName: string;
-  props: Record<string, any>;
+    templateName: string;
+    props: Record<string, any>;
 };
 
 export default class EmailTask extends BaseTask<Props> {
-  public async perform({ templateName, props, ...metadata }: Props) {
-    const EmailClass = emails[templateName];
-    if (!EmailClass) {
-      throw new Error(
-        `Email task "${templateName}" template does not exist. Check the file name matches the class name.`
-      );
-    }
+    public async perform({ templateName, props, ...metadata }: Props) {
+        const EmailClass = emails[templateName];
+        if (!EmailClass) {
+            Logger.info(
+                'email',
+                `Avaiable email template: ${Object.keys(emails).join(', ')}`
+            );
+            throw new Error(
+                `Email task "${templateName}" template does not exist. Check the file name matches the class name.`
+            );
+        }
 
-    const email = new EmailClass(props, metadata);
-    return email.send();
-  }
+        const email = new EmailClass(props, metadata);
+        return email.send();
+    }
 }
